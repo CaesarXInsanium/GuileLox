1ff3764202a5b173a57ee6ad237712a4e41b6f5b
Caesar X Insanium <caesarxinsanium.dev@gmail.com>
2023-01-31 20:24:55 -0600

    not focused on issue of looping

--- GuileLox.scm
+++ GuileLox.scm
@@ -1,18 +1,152 @@
-#!/bin/env -S guile -e main -s
+#!/usr/bin/env -S guile -e main -s
 !#
 
 ;; Usage String and Text Based Procedures with Ports
 (use-modules (ice-9 textual-ports))
-;; Scanner Class
-(load "GuileLox/scanner.scm")
+;; allows for defining records
+(use-modules (srfi srfi-9))
+;; allows for defining custome printer
+(use-modules (srfi srfi-9 gnu))
 
-;; Error Handling
-(load "GuileLox/error.scm")
+(define lox-error? #f)
 
+(define (lox-error line message)
+  (report line "" message))
+
+(define (report line where message)
+  (let ((stderr (current-error-port)))
+    (begin (display "[line " stderr)
+           (display line stderr)
+           (display "] Error" stderr)
+           (display where stderr)
+           (display ": " stderr)
+           (display message stderr)
+           (set! lox-error? #t)
+           (if lox-error?
+               -1
+               0))))
 ;; Represents STDIN when promptin user for input, this is a Guile Port
 (define STDIN (current-input-port))
 (define STDOUT (current-output-port))
 
+(define-record-type token
+  (make-token type lexeme object line)
+  token?
+  (type token-type)
+  (lexeme token-lexeme)
+  (object token-object)
+  (line token-line))
+
+(set-record-type-printer! token (lambda (record port)
+                                  (display "Token->")
+                                  (display "Type: ")
+                                  (display (token-type record))
+                                  (display " Lexeme: ")
+                                  (display (token-lexeme record))
+                                  (display " Object: ")
+                                  (display (token-object record))
+                                  (display " LineNum: ")
+                                  (display (token-line record))
+                                  (newline)))
+
+(define token-types #("LEFT_PAREN"
+                      "RIGHT_PAREN"
+                      "LEFT_BRACE"
+                      "RIGHT_BRACE"
+                      "COMMA"
+                      "DOT"
+                      "MINUS"
+                      "PLUS"
+                      "SEMICOLON"
+                      "SLASH"
+                      "STAR"
+
+                      ;; Two Char
+                      "BANG"
+                      "BANG_EQUAL"
+                      "EQUAL"
+                      "EQUAL_EQUAL"
+                      "GREATER"
+                      "GREATER_EQUAL"
+                      "LESS"
+                      "LESS_EQUAL"
+
+                      ;; Literals
+                      "IDENTIFIER"
+                      "STRING"
+                      "NUMBER"
+
+                      ;; KEYWORDS
+                      "AND"
+                      "CLASS"
+                      "ELSE"
+                      "FALSE"
+                      "FUN"
+                      "FOR"
+                      "IF"
+                      "NIL"
+                      "OR"
+                      "PRINT"
+                      "RETURN"
+                      "SUPER"
+                      "THIS"
+                      "TRUE"
+                      "VAR"
+                      "WHILE"
+                      "EOF"))
+
+(define START 0)
+(define CURRENT 0)
+(define LINE 0)
+(define SOURCELEN 0)
+
+(define (alpha? char)
+  (or (or (and (char>=? char #\a) 
+               (char<=? char #\z)) 
+          (and (char>=? char #\A)
+               (char<=? char #\Z)))
+      (char=? char #\_)))
+
+(define (digit? char)
+  (and (char>=? char #\0) (char<=? char #\0)))
+
+(define (alpha-numberic? char)
+  (or (alpha? char) (digit? char)))
+
+(define (at-end?)
+  (>= CURRENT SOURCELEN))
+
+;; Returns token type based on single char returns empty string otherwise
+(define (char-tokentype char)
+  (cond ((char=? char #\() (vector-ref token-types 0))
+        ((char=? char #\)) (vector-ref token-types 1))
+        ((char=? char #\{) (vector-ref token-types 2))
+        ((char=? char #\}) (vector-ref token-types 3))
+        ((char=? char #\,) (vector-ref token-types 4))
+        ((char=? char #\.) (vector-ref token-types 5))
+        ((char=? char #\-) (vector-ref token-types 6))
+        ((char=? char #\+) (vector-ref token-types 7))
+        ((char=? char #\;) (vector-ref token-types 8))
+        ((char=? char #\\) (vector-ref token-types 9))
+        ((char=? char #\*) (vector-ref token-types 10))
+        (else " ")))
+
+;; Returns token-type object
+(define (scan-token chr)
+  (make-token (char-tokentype chr) chr #f LINE))
+
+
+;; take in source string and return list of token records
+(define (check-types char)
+  (display "Is char?: ")
+  (display (char? char))
+  (newline))
+
+(define (scan-tokens source)
+  (begin (set! SOURCELEN (string-length source))
+         (map (lambda (char) (begin (set! START CURRENT)
+                                    (scan-token char)))
+              (string->list source))))
 (define (run source)
   (if (string? source)
       (display "Is a string!\n")
@@ -20,7 +154,7 @@
   (display "Running ")
   (display source)
   (newline)
-  (print-char source)
+  (display (scan-tokens source))
   (newline))
 
 (define (run-file path)
